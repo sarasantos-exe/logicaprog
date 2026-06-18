6c3e48ad1d7633b1af883afcbb9f650e57c5bada
Sara Ketlyn Domingues Santos <saraketlyn.312@gmail.com>
2026-06-18 11:52:25 -0300

    Exemplo do while

---
+++ l052q02.js
@@ -0,0 +1,13 @@
+//2) Desenvolver um programa que apresente no final a soma dos valores pares existentes na faixa de 0 até 500.
+//Utilize um laço que varie de 2 em 2.
+
+let cont = 0;
+let acum = 0;
+
+do {
+    acum = acum + cont; // ou "acum += cont;"
+    cont = cont + 2; // ou "cont +=2;"
+
+} while (cont <= 500);
+
+console.log (`A soma dos valores pares existentes na faixa de 0 a 500 é ${acum}.`);
